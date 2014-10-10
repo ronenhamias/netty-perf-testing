3dcad08232285a2ad0e0b0ae6c49f0ee23a4488d
Ronen Hamias <ronen.hamias@gmail.com>
2014-10-10 20:07:04 +0300

    OSValidator and Epoll support

--- netty4-worldclock/src/main/java/io/netty/example/netty4/ServerFactory.java
+++ netty4-worldclock/src/main/java/io/netty/example/netty4/ServerFactory.java
@@ -7,26 +7,36 @@ import io.netty.channel.DefaultMessageSizeEstimator;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.FixedRecvByteBufAllocator;
 import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.example.common.OSValidator;
 
 /**
  * @author Anton Kharenko
  */
 public class ServerFactory {
 
+	private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+    private Class channelClass = NioServerSocketChannel.class;
+    
+	protected void initGroups() {
+        if (OSValidator.isUnix()) {
+            bossGroup = new EpollEventLoopGroup(Runtime.getRuntime().availableProcessors()*4);
+            workerGroup = new EpollEventLoopGroup(Runtime.getRuntime().availableProcessors()*4);
+            channelClass =  EpollServerSocketChannel.class;
+        } else {
+            bossGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors()*4);
+            workerGroup = new NioEventLoopGroup(Runtime.getRuntime().availableProcessors()*4);
+        }
+    }
+	
 	public void bind(int port) {
 		final ServerBootstrap bootstrap = new ServerBootstrap();
-		
-		EventLoopGroup group;
-		if(System.getProperty("os.name").startsWith("Windows"))
-			group = new NioEventLoopGroup();
-		else
-			group = new EpollEventLoopGroup();
-		
-		
-		bootstrap.group( group)
-				.channel(NioServerSocketChannel.class)
+
+		bootstrap.group( bossGroup,workerGroup)
+				.channel(channelClass)
 				.childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
 				.childOption(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(20))
 				.childOption(ChannelOption.TCP_NODELAY, true)
